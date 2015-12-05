dec371f1ae54809f051c93a341843f04343d6828
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-12-05 20:42:23 +0200

    Make alphaBeta a standalone function

--- Negamax.hs
+++ Negamax.hs
@@ -13,14 +13,16 @@ class GamePosition a where
 negamax :: GamePosition a => Int -> Float -> Float -> Int -> a -> Float
 negamax depth a b color pos
     | depth == 0 || null nodes = fromIntegral color * evaluate pos
-    | otherwise = alphaBeta a (-1/0.0) nodes
+    | otherwise = alphaBeta depth a b color (-1/0) nodes
     where nodes = children pos
-          alphaBeta :: GamePosition a => Float -> Float -> [a] -> Float
-          alphaBeta _  val [] = val
-          alphaBeta a2 val (p:ps)
-              | val >= b || null ps = val
-              | otherwise = alphaBeta (max a2 newVal) (max val newVal) ps
-              where newVal = -negamax (depth-1) (-b) (-a2) (-color) p
+
+alphaBeta :: GamePosition a =>
+    Int -> Float -> Float -> Int -> Float -> [a] -> Float
+alphaBeta _ _ _ _ val [] = val
+alphaBeta depth a b color val (p:ps)
+    | val >= b = val
+    | otherwise = alphaBeta depth (max a newVal) b color (max val newVal) ps
+    where newVal = -negamax (depth-1) (-b) (-a) (-color) p
 
 -- The initial call to negamax which returns the chosen position along with the
 -- score
