9bf170cbae20adfc43ecc80518a2c585224ba485
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-12-07 22:38:50 +0200

    Wording of comments

--- Negamax.hs
+++ Negamax.hs
@@ -19,7 +19,7 @@ negamax depth a b color pos
     where nodes = children pos
 
 -- Let's have some fun with the Either monad in the alpha-beta pruning algorithm
--- Here Left means that the pruning is finished
+-- Left means that the pruning is finished
 -- Right means that the search is going on
 type AlphaBeta = Either Float Float
 
@@ -31,27 +31,29 @@ fromAB (Left a)  = a
 -- Now the alpha-beta pruning can be nicely expressed as a fold in terms
 -- of the AlphaBeta monad. The fold _could_ be implemented equally well in
 -- without the monad, but the abstraction makes the control flow somewhat
--- clearer by stating explicitly that the pruning is finished in the middle
+-- clearer by stating explicitly if the pruning is finished in the middle
 -- of the fold.
 alphaBeta :: GamePosition a => Int -> Float -> Float -> Int -> [a] -> Float
 alphaBeta depth a b c ps = fromAB $ foldM f (-1/0) ps
     where f :: GamePosition a => Float -> a -> AlphaBeta
           f val pos
-              | val >= b  = Left val  -- could return plain Float values here
-                                      -- as well and it would work, but wouldn't
-                                      -- be as neat
+              | val >= b  = Left val  -- could use a regular fold and return
+                                      -- plain Float values here as well and it
+                                      -- would work, but wouldn't be as neat
+                                      -- and explicit about where and how the
+                                      -- pruning is finished
               | otherwise = Right $ max val newVal
               where newVal = -negamax (depth-1) (-b) (-max a val) (-c) pos
 
 -- The initial call to negamax which returns the chosen position along with the
 -- score
 bestNextPosition :: GamePosition a => Int -> a -> a
-bestNextPosition depth pos = snd . ab (-1/0.0) (-1/0.0, pos) $ children pos
+bestNextPosition depth pos = snd . ab (-1/0) (-1/0, pos) $ children pos
     where ab :: GamePosition a => Float -> (Float, a) -> [a] -> (Float, a)
           ab _ x [] = x
           ab a oldPos@(val, _) (p:ps)
-              | val >= 1/0.0 || null ps = oldPos
+              | val >= 1/0 = oldPos
               | otherwise = ab (max a newVal) newPos ps
-              where newVal = -negamax (depth-1) (-1/0.0) (-a) (-1) p
+              where newVal = -negamax (depth-1) (-1/0) (-a) (-1) p
                     newPos = if newVal > val then (newVal, p) else oldPos
 
